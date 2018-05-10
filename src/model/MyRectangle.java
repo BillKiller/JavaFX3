89a5c3fba1fa36fa14d33713d08e74d28edc9797
billkiller <747441355@qq.com>
2018-05-11 01:08:00 +0800

    修复了矩形bug

--- src/model/MyRectangle.java
+++ src/model/MyRectangle.java
@@ -3,61 +3,40 @@ package model;
 import java.text.DecimalFormat;
 
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 
-public class MyRectangle extends  MyShape {
-    private Rectangle rectangle;
-    public MyRectangle(double x,double y,int id){
-        this(x,y,100,50);
-        this.factoryID=id;
-    }
-    public MyRectangle(double x,double y,double width,double height){
-        super(x,y,width,height);
-        this.rectangle=new Rectangle(x-width,y-height,2*width,2*height);
-        this.rectangle.setFill(Color.WHITE);
-        this.rectangle.setStroke(Color.BLACK);
-        super.setMyShape(this.rectangle);
-    }
-    public void setX(double x){
-    	this.x =x;
-    	 updateRectangle();
-        rectangle.setX(x-width);
-    }
-    public void setY(double y){
-        this.y = y;
-        updateRectangle();
-    	rectangle.setY(y-height);
-    }
-    public void setWidth(double width){
-        this.width=width;
-        this.x = leftX+width;
-    	this.y = leftY+height;
-        rectangle.setWidth(2*width);
-        updateRectangle();
-    }
-    public  void setHeight(double height){
-    	this.height=height;
-    	this.x = leftX+width;
-    	this.y = leftY+height;
-    	rectangle.setHeight(2*height);
-        updateRectangle();
-    }
-    @Override
-    public void Move(double x,double y){
-        super.Move(x,y);
-        rectangle.setX(x-width);
-        rectangle.setY(y-height);
-    }
-    public void updateRectangle(){
-    		this.leftX = x -width;
-    		this.leftY = y - height;
-    		updateLocation(this.x, this.y);
-    }
+public class MyRectangle extends  MyPolygon {
 
-	public String toString() {
-		DecimalFormat df = new DecimalFormat("#.000");
-		String tostring = getClass().getSimpleName() + "< "+factoryID+" >"+ "(" + df.format(this.x) + "," + df.format(this.y) + "," + df.format(this.width) + ","
-				+ df.format(this.height) + ")" + "[ " + text.getText() + " ]" +"{"+getCSS()+"}"+" ;\n";
-		return tostring;
+	public MyRectangle(double x, double y,int id) {
+		super(x, y, 100,50);
+		this.factoryID=id;
+		polygon = new Polygon();
+		setMyShape(polygon);
+		setShape();
 	}
+	public MyRectangle(double x, double y,double witdh,double height,int id) {
+			super(x, y, witdh, height);
+			this.factoryID = id;
+	}
+	@Override
+	public void setShape() {
+		double upLeftX = this.x-width;
+		double upLeftY = this.y - height;
+		double upRightX = this.x + width;
+		double upRightY = this.y-height;
+		double downLeftX = this.x - width;
+		double downLeftY = this.y + height;
+		double downRightX = this.x + width;
+		double downRightY = this.y + height;
+		Double[] list = { upLeftX, upLeftY, upRightX, upRightY, downRightX, downRightY, downLeftX, downLeftY };
+		polygon.getPoints().setAll(list);
+	}
+
+		public String toString() {
+			DecimalFormat df = new DecimalFormat("#.000");
+			String tostring = getClass().getSimpleName() + "< "+factoryID+" >"+ "(" + df.format(this.x) + "," + df.format(this.y) + "," + df.format(this.width) + ","
+					+ df.format(this.height) + ")" + "[ " + text.getText() + " ]" +"{"+getCSS()+"}"+" ;\n";
+			return tostring;
+		}
 }
